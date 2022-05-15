9e895ade0c974b4e1d00e134cb1e39352168f44f
Cynthia Amaji <cynthyok4luv@gmail.com>
2022-05-15 09:39:10 +0100

    main function file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,26 +1,44 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - main file
- * @argc: number of lines arguments
- * @argv: array of elements
- * Return: 0
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
  */
 
 int main(int argc, char *argv[])
 {
-	int R;
+	char *sign;
+	char operator;
+	int a, b, answer;
+	int (*ptr)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	R = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
-	printf("%d\n", R);
-
+	operator = argv[2][0];
+	if ((operator != '*' && operator != '+' && operator != '-'
+	     && operator != '/'  && operator != '%') || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if ((operator == '/' || operator == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	sign = argv[2];
+	ptr = get_op_func(sign);
+	answer = (*ptr)(a, b);
+	printf("%d\n", answer);
 	return (0);
 }
