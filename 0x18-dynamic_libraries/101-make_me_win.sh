f7cdbd0e2b5167f73e02263e29faa5d7a5a883d3
Cynthia Amaji <cynthyok4luv@gmail.com>
2022-07-19 23:22:08 +0100

    3. giga millions program

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./gm 9 8 10 24 75 9
+wget -P /tmp/ https://raw.github.com/luischaparroc/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
